8f78f487df25236030d0588d17a87e5dc896d14d
Ankit Vani <a@nevitus.org>
2016-11-07 16:08:27 -0500

    added rnncell

---
+++ rnncell.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+
+import utils
+
+
+class GRUCell(tf.nn.rnn_cell.RNNCell):
+
+    """Gated Recurrent Unit cell (cf. http://arxiv.org/abs/1406.1078).
+       This variant can be conditioned on a provided latent variable.
+       Based on the code from TensorFlow."""
+
+    def __init__(self, num_units, latent=None, activation=tf.nn.tanh):
+        self.num_units = num_units
+        self.latent = latent
+        self.activation = activation
+
+    @property
+    def state_size(self):
+        return self.num_units
+
+    @property
+    def output_size(self):
+        return self.num_units
+
+    def __call__(self, inputs, state, scope=None):
+        """Gated recurrent unit (GRU) with num_units cells."""
+        with tf.variable_scope(scope or type(self).__name__):  # "GRUCell"
+            with tf.variable_scope("Gates"):  # Reset gate and update gate.
+                # We start with bias of 1.0 to not reset and not update.
+                factors = [inputs, state]
+                if self.latent is not None:
+                    factors.append(self.latent)
+                r, u = tf.split(1, 2, utils.linear(factors, 2 * self.num_units, True, 1.0))
+                r, u = tf.nn.sigmoid(r), tf.nn.sigmoid(u)
+            with tf.variable_scope("Candidate"):
+                factors = [inputs, r * state]
+                if self.latent is not None:
+                    factors.append(self.latent)
+                c = self.activation(utils.linear(factors, self.num_units, True))
+            new_h = u * state + (1 - u) * c
+        return new_h, new_h
