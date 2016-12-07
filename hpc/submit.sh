7714d2c8c890af273f4a33418d30ca5fc2c64327
Vighnesh Birodkar <vighneshbirodkar@nyu.edu>
2016-12-07 02:18:55 -0500

    changed priniting in submit.sh

--- hpc/submit.sh
+++ hpc/submit.sh
@@ -8,5 +8,4 @@ echo "Arguments = $DL_ARGS"
 
 USER_NAME=$(whoami)
 
-echo "$USER_NAME@nyu.edu"
 qsub -N $JOB_NAME -v DL_ARGS -l nodes=1:ppn=2:gpus=1,walltime=47:59:00,pmem=8GB -m abe -M "$USER_NAME@nyu.edu" senthing.sh
