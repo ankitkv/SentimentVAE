e7c67b54289e2b59eeba8808b8808e31ec07d54f
Vighnesh Birodkar <vighneshbirodkar@nyu.edu>
2016-12-07 23:58:02 -0500

    write log files in log folder

--- hpc/submit.sh
+++ hpc/submit.sh
@@ -8,4 +8,4 @@ echo "Arguments = $DL_ARGS"
 
 USER_NAME=$(whoami)
 
-qsub -N $JOB_NAME -v DL_ARGS -l nodes=1:ppn=2:gpus=1,walltime=47:59:00,pmem=8GB -m abe -M "$USER_NAME@nyu.edu" senthing.sh
+qsub -N $JOB_NAME -v DL_ARGS -l nodes=1:ppn=2:gpus=1,walltime=47:59:00,pmem=8GB -m abe -M "$USER_NAME@nyu.edu" senthing.sh -o ./logs/$JOB_NAME.out -e ./logs/$JOB_NAME.err
