61bea82ba7c397a2565b363f18aeba689d45861f
Vighnesh Birodkar <vighneshbirodkar@nyu.edu>
2016-12-08 00:06:58 -0500

    make log dir in script

--- hpc/submit.sh
+++ hpc/submit.sh
@@ -8,4 +8,6 @@ echo "Arguments = $DL_ARGS"
 
 USER_NAME=$(whoami)
 
+mkdir ./logs
+
 qsub -N $JOB_NAME -v DL_ARGS -l nodes=1:ppn=2:gpus=1,walltime=47:59:00,pmem=8GB -m abe -M "$USER_NAME@nyu.edu" senthing.sh -o ./logs/$JOB_NAME.out -e ./logs/$JOB_NAME.err
